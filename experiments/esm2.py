ada8673b30a7ce25dcb09b433345b06b7dbd91a7
Marc Singleton <mdsingl@sandia.gov>
2026-06-03 23:08:47 -0700

    Added MLflow logging

--- experiments/esm2.py
+++ experiments/esm2.py
@@ -1,8 +1,10 @@
 """Simple fine-tuning of ESM2 by training new prediction head."""
 
+import tomllib
 from functools import partial
 from pathlib import Path
 
+import mlflow
 import torch
 import torch.nn as nn
 import torchmetrics
@@ -81,81 +83,10 @@ def esm_collate_generator(samples, esm_batch_converter):
     return tokens, labels
 
 
-cluster_name = 'clusters_85'
-train_path = Path(f'data/processed/splits/{cluster_name}/train.csv')
-val_path = Path(f'data/processed/splits/{cluster_name}/val.csv')
-
-id_col = 'split_id'
-# fmt: off
-seq_cols = [
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
-    '13', '14', '15', '16', '17', '17A', '18', '19', '20', '21', '22',
-    '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33',
-    '34', '35', '36', '37', '38', '39', '40', '41', '41A', '42', '42A',
-    '42B', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52',
-    '53', '54', '55', '56', '57', '58', '59', '60', '60A', '61A', '61',
-    '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72',
-    '73', '74', '75', '76', '76A', '77', '77A', '78', '79', '79A',
-    '80', '81', '82', '83', '84', '85', '85A', '86', '87', '88', '89',
-    '90', '91', '92', '93', '94', '95', '96', '97', '97A', '97B',
-    '97C', '97D', '98', '98A', '98B', '99', '99A', '99B', '100',
-    '100A', '101', '101A', '101B', '101C', '101D', '101E', '101F',
-    '102', '103', '104', '105', '106', '107', '108',
-    '109', '110', '111', '111A', '111B', '111C', '111D', '111E',
-    '111F', '111G', '111H', '112I', '112H', '112G', '112F', '112E',
-    '112D', '112C', '112B', '112A', '112', '113', '114', '115', '116',
-    '117', '118', '119', '120', '121', '122', '123', '124', '125',
-    '126', '127', '128',
-]
-# fmt: on
-
-experiment_name = 'esm2_finetune_head'
-run_name = 'initial_test'
-batch_size = 128
-epoch_num = 100
-log_interval = 10
-
-if __name__ == '__main__':
-    esm2_trunk, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
-
-    model = ESM2Classifier(esm2_trunk)
-    for param in model.esm2_trunk.parameters():
-        param.requires_grad = False
-
-    if torch.accelerator.is_available():
-        device = torch.accelerator.current_accelerator()
-    else:
-        device = torch.device('cpu')
-
-    model = model.to(device)
-
-    optimizer = torch.optim.Adam(model.parameters())
-
-    loss_fn = torch.nn.BCELoss()
-
-    batch_converter = alphabet.get_batch_converter()
-    collate_fn = partial(esm_collate_generator, esm_batch_converter=batch_converter)
-
-    train_data = AlignedSequencesFromCSVDataset(train_path, id_col, seq_cols)
-    train_dataloader = DataLoader(
-        train_data, batch_size=batch_size, drop_last=True, shuffle=True, collate_fn=collate_fn
-    )
-    val_data = AlignedSequencesFromCSVDataset(val_path, id_col, seq_cols)
-    val_dataloader = DataLoader(
-        val_data, batch_size=batch_size, drop_last=True, shuffle=True, collate_fn=collate_fn
-    )
-
-    acc_metric = torchmetrics.classification.BinaryAccuracy().to(device)
-    recall_metric = torchmetrics.classification.BinaryRecall().to(device)
-    precision_metric = torchmetrics.classification.BinaryPrecision().to(device)
-
-    # Training loop
+def train():
     step_idx = 0
-    logs = []
     for epoch_idx in range(epoch_num):
         # Train epoch
-        loss = 0
-        num_samples = 0
         model.train()
         with tqdm.trange(
             len(train_dataloader), unit='batch', bar_format='{l_bar}{bar:10}{r_bar}'
@@ -172,38 +103,33 @@ if __name__ == '__main__':
                 step_loss.backward()
                 optimizer.step()
 
-                acc = acc_metric(output, target)
+                loss_metric(step_loss, len(input))
+                acc_metric(output, target)
                 recall_metric(output, target)
                 precision_metric(output, target)
 
-                loss += step_loss * len(input)
-                num_samples += len(input)
                 if step_idx % log_interval == 0:
-                    logs.append(
-                        {
-                            'epoch_idx': epoch_idx,
-                            'step_idx': step_idx,
-                            'step_loss': step_loss.item(),
-                        }
-                    )
+                    mlflow.log_metric('step_loss', step_loss.item(), step=step_idx)
                 bar.set_postfix(loss=step_loss.item())
 
                 bar.update(1)
                 step_idx += 1
 
-        log = {'epoch_num': epoch_idx, 'step_idx': step_idx, 'loss': (loss / num_samples).item()}
+        loss = loss_metric.compute().item()
+        loss_metric.reset()
+        mlflow.log_metric('loss_train', loss, step=step_idx)
 
         acc = acc_metric.compute().item()
         acc_metric.reset()
-        log['acc_train'] = acc
+        mlflow.log_metric('acc_train', acc, step=step_idx)
 
         recall = acc_metric.compute().item()
         recall_metric.reset()
-        log['recall_train'] = recall
+        mlflow.log_metric('recall_train', recall, step=step_idx)
 
         precision = precision_metric.compute().item()
         precision_metric.reset()
-        log['precision_train'] = precision
+        mlflow.log_metric('precision_train', precision, step=step_idx)
 
         # Validation
         outputs = []
@@ -214,20 +140,98 @@ if __name__ == '__main__':
                 target = target.to(device)
                 output = model(input).squeeze()
 
+                step_loss = loss_fn(output, target)
+
+                loss_metric(step_loss, len(input))
                 acc_metric(output, target)
                 recall_metric(output, target)
                 precision_metric(output, target)
 
+        loss = loss_metric.compute().item()
+        loss_metric.reset()
+        mlflow.log_metric('loss_val', loss, step=step_idx)
+
         acc = acc_metric.compute().item()
         acc_metric.reset()
-        log['acc_val'] = acc
+        mlflow.log_metric('acc_val', acc, step=step_idx)
 
         recall = acc_metric.compute().item()
         recall_metric.reset()
-        log['recall_val'] = recall
+        mlflow.log_metric('recall_val', recall, step=step_idx)
 
         precision = precision_metric.compute().item()
         precision_metric.reset()
-        log['precision_val'] = precision
+        mlflow.log_metric('precision_val', precision, step=step_idx)
 
-        logs.append(log)
+
+cluster_name = 'clusters_85'
+train_path = Path(f'data/processed/splits/{cluster_name}/train.csv')
+val_path = Path(f'data/processed/splits/{cluster_name}/val.csv')
+
+id_col = 'split_id'
+with open('config.toml', 'rb') as file:
+    config = tomllib.load(file)
+seq_cols = config['constants']['imgt_columns']['all_polyreactivity']
+
+experiment_name = 'esm2_finetune_head'
+run_name = 'initial_test'
+esm2_trunk_name = 'esm2_t33_650M_UR50D'
+batch_size = 128
+epoch_num = 100
+lr = 0.001
+log_interval = 10
+
+if __name__ == '__main__':
+    esm2_trunk, alphabet = esm.pretrained.load_model_and_alphabet(esm2_trunk_name)
+
+    model = ESM2Classifier(esm2_trunk)
+    for param in model.esm2_trunk.parameters():
+        param.requires_grad = False
+
+    if torch.accelerator.is_available():
+        device = torch.accelerator.current_accelerator()
+    else:
+        device = torch.device('cpu')
+
+    model = model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    loss_fn = torch.nn.BCELoss()
+
+    batch_converter = alphabet.get_batch_converter()
+    collate_fn = partial(esm_collate_generator, esm_batch_converter=batch_converter)
+
+    train_data = AlignedSequencesFromCSVDataset(train_path, id_col, seq_cols)
+    train_dataloader = DataLoader(
+        train_data, batch_size=batch_size, drop_last=True, shuffle=True, collate_fn=collate_fn
+    )
+    val_data = AlignedSequencesFromCSVDataset(val_path, id_col, seq_cols)
+    val_dataloader = DataLoader(
+        val_data, batch_size=batch_size, drop_last=True, shuffle=True, collate_fn=collate_fn
+    )
+
+    loss_metric = torchmetrics.aggregation.MeanMetric().to(device)
+    acc_metric = torchmetrics.classification.BinaryAccuracy().to(device)
+    recall_metric = torchmetrics.classification.BinaryRecall().to(device)
+    precision_metric = torchmetrics.classification.BinaryPrecision().to(device)
+
+    mlflow.set_experiment(experiment_name)
+    mlflow.enable_system_metrics_logging()
+
+    with mlflow.start_run(run_name=run_name):
+        mlflow.set_tag('cluster_name', cluster_name)
+        mlflow.log_params(
+            {
+                'esm2_trunk_name': esm2_trunk_name,
+                'batch_size': batch_size,
+                'epoch_num': epoch_num,
+                'lr': lr,
+            }
+        )
+
+        train()
+
+        mlflow.pytorch.log_model(
+            pytorch_model=model.classifier_head, name=f'{experiment_name}-final'
+        )
